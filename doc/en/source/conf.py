ec4d24718a3c54ffee84299dacbe67e7304776d3
Hiroya-W <hyuyukun@gmail.com>
2022-10-27 09:11:57 +0000

    Fix qulacs src folder path

--- doc/en/source/conf.py
+++ doc/en/source/conf.py
@@ -52,7 +52,7 @@ exhale_args = {
     # "treeViewIsBootstrap": True,
     "exhaleExecutesDoxygen": True,
     "exhaleDoxygenStdin": \
-        "INPUT = ../../qulacs/src/cppsim ../../qulacs/src/vqcsim \n \
+        "INPUT = ../../../src/cppsim ../../../src/vqcsim \n \
         FILE_PATTERNS          = *.hpp \n \
         WARN_IF_UNDOCUMENTED   = NO \n \
         ENABLE_PREPROCESSING   = YES \n \
@@ -77,6 +77,7 @@ exhale_args = {
 
 # on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
 import os
+
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
 if not on_rtd:  # only import and set the theme if we're building docs locally
